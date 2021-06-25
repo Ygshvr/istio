305bb35b6bf02fc30de798742ffe4a55cc26ff08
Yogeshwar Singh <yogeshwar@infracloud.io>
2021-06-25 10:59:11 +0530

    updated the end-to-end as per new code changes.

--- tetrateci/tsb-util/e_to_e_test.py
+++ tetrateci/tsb-util/e_to_e_test.py
@@ -16,10 +16,11 @@ def are_dir_trees_equal(fixtures_dir, generated_dir):
         False otherwise.
    """
 
-    dirs_cmp = filecmp.dircmp(fixtures_dir, generated_dir)
+    dirs_cmp = filecmp.dircmp(fixtures_dir, generated_dir, ignore=['cert'])
     if len(dirs_cmp.left_only)>0 or len(dirs_cmp.right_only)>0 or \
         len(dirs_cmp.funny_files)>0:
         print('Mismatch in number of files.')
+        dirs_cmp.report()
         return False
     (_, mismatch, errors) =  filecmp.cmpfiles(
         fixtures_dir, generated_dir, dirs_cmp.common_files, shallow=False)
@@ -36,35 +37,37 @@ def are_dir_trees_equal(fixtures_dir, generated_dir):
 
 
 def main():
-    
-    if(os.path.isdir('./generated')):
-        shutil.rmtree('./generated')
+    generated_folder = './generated'
+    if(os.path.isdir(generated_folder)):
+        shutil.rmtree(generated_folder)
+    shutil.copytree('./fixtures/test_cert', generated_folder + '/cert')
     
     # testing tsb_util.py
-    os.system('python tsb_util.py --config ./fixtures/general-config.yml')
-    assert are_dir_trees_equal('./fixtures/general_generated', './generated') == True, 'tsb_utils.py test failed.'
+    os.system('python tsb_util.py --config ./fixtures/general-config.yml --folder ' + generated_folder)
+    assert are_dir_trees_equal('./fixtures/general_generated', generated_folder) == True, 'tsb_utils.py test failed.'
     print('>> 1. tsb_utils test completed successfully.')
     # Doing clean up
-    shutil.rmtree('./generated')
-    
+    shutil.rmtree(generated_folder)
+    shutil.copytree('./fixtures/test_cert', generated_folder + '/cert')
     # testing single_ns.py
-    os.system('python single_ns.py --config ./fixtures/httpbin-config.yml')
-    assert are_dir_trees_equal('./fixtures/httpbin_generated', './generated') == True, 'single_ns.py test failed.'
+    os.system('python single_ns.py --config ./fixtures/httpbin-config.yml --folder ' + generated_folder)
+    assert are_dir_trees_equal('./fixtures/httpbin_generated', generated_folder) == True, 'single_ns.py test failed.'
     print('>> 2. single_ns.py test completed successfully.')
      # Doing clean up
-    shutil.rmtree('./generated')
-
+    shutil.rmtree(generated_folder)
+    shutil.copytree('./fixtures/test_cert', generated_folder + '/cert')
     # testing bookinfo-single-gw.py
-    os.system('python bookinfo-single-gw.py --config ./fixtures/bookinfo-single-direct.yml')
-    assert are_dir_trees_equal('./fixtures/bookinfo_single_direct_generated', './generated') == True, 'bookinfo-single.yaml test failed in direct mode.'
+    os.system('python bookinfo-single-gw.py --config ./fixtures/bookinfo-single-direct.yml --folder ' + generated_folder)
+    assert are_dir_trees_equal('./fixtures/bookinfo_single_direct_generated', generated_folder) == True, 'bookinfo-single.yaml test failed in direct mode.'
     print('>> 3.1 bookinfo-single.yaml direct mode test completed successfully.')
      # Doing clean up
-    shutil.rmtree('./generated')
-    os.system('python bookinfo-single-gw.py --config ./fixtures/bookinfo-single-bridged.yml')
-    assert are_dir_trees_equal('./fixtures/bookinfo_single_bridged_generated', './generated') == True, 'bookinfo-single.yaml test failed in bridged mode.'
+    shutil.rmtree(generated_folder)
+    shutil.copytree('./fixtures/test_cert', generated_folder + '/cert')
+    os.system('python bookinfo-single-gw.py --config ./fixtures/bookinfo-single-bridged.yml --folder ' + generated_folder)
+    assert are_dir_trees_equal('./fixtures/bookinfo_single_bridged_generated', generated_folder) == True, 'bookinfo-single.yaml test failed in bridged mode.'
     print('>> 3.2 bookinfo-single.yaml bridged mode test completed successfully.')
      # Doing clean up
-    shutil.rmtree('./generated')
+    shutil.rmtree(generated_folder)
     
 if __name__ == "__main__":
     main()
